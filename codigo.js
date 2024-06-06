0cd0c60c126f8cb2c20474a49f327894e8dc67aa
isa <160788613+laraaawr7@users.noreply.github.com>
2024-06-06 20:20:00 -0300

    a

---
+++ codigo.js
@@ -0,0 +1,8 @@
+function acordajoe() {
+    let resposta; //criando a variavel resposta, let cria uma variável//
+    resposta = prompt("Joe, vamos jogar?");
+    if (resposta == "y") {
+        alert("vamos jogar")
+    } else
+        alert("vá catar morangos")
+}
